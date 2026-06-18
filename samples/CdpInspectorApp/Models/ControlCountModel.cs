506381a84ec9f3ec1b2ff1a02fd0f8c0fdd97976
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-06-18 13:19:33 +0200

    refactor(inspector): update CdpInspectorApp to consume shared library and remove local copy

--- samples/CdpInspectorApp/Models/ControlCountModel.cs
+++
@@ -1,7 +0,0 @@
-namespace CdpInspectorApp.Models;
-
-public class ControlCountModel
-{
-    public string Type { get; set; } = "";
-    public int Count { get; set; }
-}
--- samples/CdpInspectorApp/Models/StorageEntryModel.cs
+++
@@ -1,7 +0,0 @@
-namespace CdpInspectorApp.Models;
-
-public class StorageEntryModel
-{
-    public string Key { get; set; } = "";
-    public string Value { get; set; } = "";
-}
