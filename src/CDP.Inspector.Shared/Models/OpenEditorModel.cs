4e02a3316e1990460175807a2f82c88c8b5fbe47
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-11 14:40:22 +0200

    feat(inspector/models): Add OpenEditorModel and update models for project workspace search

---
+++ src/CDP.Inspector.Shared/Models/OpenEditorModel.cs
@@ -0,0 +1,42 @@
+using CdpInspectorApp.ViewModels;
+
+namespace CdpInspectorApp.Models;
+
+public class OpenEditorModel : ViewModelBase
+{
+    private string _filePath = "";
+    private string _displayName = "";
+    private string _originalContent = "";
+    private bool _isDirty;
+    private bool _isActive;
+
+    public string FilePath
+    {
+        get => _filePath;
+        set => RaiseAndSetIfChanged(ref _filePath, value);
+    }
+
+    public string DisplayName
+    {
+        get => _displayName;
+        set => RaiseAndSetIfChanged(ref _displayName, value);
+    }
+
+    public string OriginalContent
+    {
+        get => _originalContent;
+        set => RaiseAndSetIfChanged(ref _originalContent, value);
+    }
+
+    public bool IsDirty
+    {
+        get => _isDirty;
+        set => RaiseAndSetIfChanged(ref _isDirty, value);
+    }
+
+    public bool IsActive
+    {
+        get => _isActive;
+        set => RaiseAndSetIfChanged(ref _isActive, value);
+    }
+}
