64dd8b1cfda4ebc5ce7113f04529747c72f85561
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-23 21:59:11 +0200

    fix(uno): configure GtkHost for macOS Skia desktop rendering

--- samples/UnoSampleApp/Platforms/Desktop/Program.cs
+++ samples/UnoSampleApp/Platforms/Desktop/Program.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Linq.Expressions;
-using System.Reflection;
 using Microsoft.UI.Xaml;
 
 namespace UnoSampleApp;
@@ -14,45 +12,12 @@ internal class Program
 
         try
         {
-            var asm = Assembly.Load("Uno.UI.Runtime.Skia.MacOS");
-            var builderType = asm.GetType("Uno.UI.Runtime.Skia.HostBuilder");
-            if (builderType != null)
-            {
-                var createMethod = builderType.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
-                var builder = createMethod?.Invoke(null, null);
-
-                if (builder != null)
-                {
-                    var appMethod = builder.GetType().GetMethod("App");
-                    builder = appMethod?.Invoke(builder, new object[] { new Func<Application>(() => new App()) });
-
-                    var useMacMethod = builder?.GetType().GetMethod("UseMacOS");
-                    if (useMacMethod != null)
-                    {
-                        var pars = useMacMethod.GetParameters();
-                        if (pars.Length > 0)
-                        {
-                            var actionType = typeof(Action<>).MakeGenericType(pars[0].ParameterType);
-                            var param = Expression.Parameter(pars[0].ParameterType, "opt");
-                            var lambda = Expression.Lambda(actionType, Expression.Empty(), param);
-                            var configureDelegate = lambda.Compile();
-
-                            builder = useMacMethod.Invoke(builder, new object[] { configureDelegate });
-                        }
-                    }
-
-                    var buildMethod = builder?.GetType().GetMethod("Build");
-                    var host = buildMethod?.Invoke(builder, null);
-
-                    var runMethod = host?.GetType().GetMethod("Run");
-                    runMethod?.Invoke(host, null);
-                    return;
-                }
-            }
+            var gtkHost = new Uno.UI.Runtime.Skia.Gtk.GtkHost(() => new App());
+            gtkHost.Run();
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Skia macOS host EXCEPTION: {ex.InnerException ?? ex}");
+            Console.WriteLine($"GTK Host error: {ex.Message}");
         }
     }
 }
